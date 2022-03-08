a079f1f52e446b62adbf4660277dc199fa013041
Ezequiel <ezequielamin@outlook.com>
2022-03-08 19:11:44 -0300

    cambios en marquee

--- src/components/Lower.jsx
+++ src/components/Lower.jsx
@@ -5,7 +5,7 @@ import "./lower.css";
 const Lower = () => {
   return (
     <div className="mb-4">
-        <p className="lower__description">
+        <p className="lower__description mb-3">
           I've recently ended my Web Development studies, and I'm looking
           forward to learning new technologies and abilities. I know I've got a
           lot ahead of me yet! But, until then, I can enumerate these skills:
--- src/components/Upper.jsx
+++ src/components/Upper.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React from "react";
 import "./upper.css";
 
 const Upper = () => {
-    return (
-        <div className='row justify-content-between upper__order'>
-            <div className='col-sm-12 col-md-7'>
-                <h1 className='fw-bold upper__title'>Hi!</h1>
-                <h3>I'm <strong>Ezequiel Amin</strong>, Argentine MERN Full Stack Developer.</h3>
-                <p className="lower__description">
+  return (
+    <div className="row justify-content-between upper__order">
+      <div className="col-sm-12 col-md-7">
+        <h1 className="fw-bold upper__title">Hi!</h1>
+        <h3>
+          I'm <strong>Ezequiel Amin</strong>, Argentine MERN Full Stack
+          Developer.
+        </h3>
+        <p className="lower__description mb-0">
           I'm studying to be a Computer Science Engineer by the end of 2025.
           Among other things, I love math and programming. I believe my studies
           mix them both greatly, and that's why I love what I study and what I
-          do. </p>
-            </div>
-            <div className='upper__profilePic col-sm-12 col-md-5'>
-                <img src="/img/eze.jpg" alt="Ezequiel Amin" />
-            </div>
-        </div>
-    );
+          do.{" "}
+        </p>
+      </div>
+      <div className="upper__profilePic col-sm-12 col-md-5">
+        <img src="/img/eze.jpg" alt="Ezequiel Amin" />
+      </div>
+    </div>
+  );
 };
 
 export default Upper;
